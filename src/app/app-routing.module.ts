9adabf268dc356d7511e28dd120bd094a1059742
Vincent Prouchet <prouchetvincent@hotmail.com>
2022-08-31 15:35:56 +0200

    first commit
    Creation des composants
    creation des routes (qui ne fonctionnent pas)
    creation de la navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductAddComponent } from "./product-add/product-add.component";
+import { ProductGetComponent } from "./product-get/product-get.component";
+import { ProductEditComponent } from "./product-edit/product-edit.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pruduct/create',
+    component: ProductAddComponent
+  },
+  {
+    path: 'edit/:id',
+    component: ProductEditComponent
+  },
+  {
+    path: 'pruducts',
+    component: ProductGetComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductGetComponent } from './product-get/product-get.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { LoadingBarModule } from "@ngx-loading-bar/core";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductAddComponent,
+    ProductGetComponent,
+    ProductEditComponent
   ],
   imports: [
     BrowserModule,
+    LoadingBarModule,
     AppRoutingModule
   ],
   providers: [],
